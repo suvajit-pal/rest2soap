01a6d78c61810f3a6b064b6461fcdde5f819072f
Suvajit Pal <supal@adobe.com>
2020-05-01 17:28:43 +0530

    Updated Licensing.

--- actions/getRestRequestFromSoap/index.js
+++ actions/getRestRequestFromSoap/index.js
@@ -1,7 +1,19 @@
+/*
+Copyright 2020 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+
+Author: Suvajit@Adobe
+*/
 /*  
- * Helper action to get REST request format from SOAP
+ * Helper action to get REST request format from a SOAP requets format.
  *
- * Suvajit@Adobe.
 */
 
 const Utils = require('../utils');
@@ -9,8 +21,6 @@ const Utils = require('../utils');
 async function main (params) {
 
   try {
-    // replace this with the api you want to access
-    // if needed apikeys and tokens can be passed to the action using default parameters and dotenv variables
 
 	const util = new Utils(params);
 	
